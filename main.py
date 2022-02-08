4eb4ff5315a0fe49a59428bf8d6c70ecf042d8db
Vicky Chen <89808845+Xiao2zi99@users.noreply.github.com>
2022-02-08 12:33:58 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -12,6 +12,7 @@ from scipy.signal import find_peaks
 import sys
 import os
 import json
+import csv
 
 def extractdata(f, channel_list):    
     i = 1
@@ -19,8 +20,17 @@ def extractdata(f, channel_list):
     tempdata_list2 = [] 
     tempdata_list3 = [] 
     tempdata_list4 = [] 
-    data_ID = []
-    data_time = []
+    
+    data_ID1 = []
+    data_ID2 = []
+    data_ID3 = []
+    data_ID4 = []
+    
+    data_time1 = []
+    data_time2 = []
+    data_time3 = []
+    data_time4 = []
+    
     data = []
     data_ch1 = []   
     data_ch2 = [] 
@@ -38,38 +48,38 @@ def extractdata(f, channel_list):
                 if channel == 1:
                     tempdata_list1.append(event.adc_data[boardID][channel]) #event.adc_data[boardID][boardChannel] -> Board nr. 3059 mit Input auf Channel 1
                     list_length = len(tempdata_list1)
-                    data_ID.append(event.event_id)
-                    data_time.append(event.timestamp)
+                    data_ID1.append(event.event_id)
+                    data_time1.append(event.timestamp)
                         
                     
-                    data_ch1 = {"data": tempdata_list1, "identity": data_ID, "time": data_time} 
+                    data_ch1 = {"data": tempdata_list1, "identity": data_ID1, "time": data_time1} 
                     
                 elif channel == 2:
                     tempdata_list2.append(event.adc_data[boardID][channel]) #event.adc_data[boardID][boardChannel] -> Board nr. 3059 mit Input auf Channel 1
                     list_length = len(tempdata_list2)
-                    data_ID.append(event.event_id)
-                    data_time.append(event.timestamp)
+                    data_ID2.append(event.event_id)
+                    data_time2.append(event.timestamp)
                         
                     
-                    data_ch2 = {"data": tempdata_list2, "identity": data_ID, "time": data_time} 
+                    data_ch2 = {"data": tempdata_list2, "identity": data_ID2, "time": data_time2} 
                     
                 elif channel == 3:
                     tempdata_list3.append(event.adc_data[boardID][channel]) #event.adc_data[boardID][boardChannel] -> Board nr. 3059 mit Input auf Channel 1
                     list_length = len(tempdata_list3)
-                    data_ID.append(event.event_id)
-                    data_time.append(event.timestamp)
+                    data_ID3.append(event.event_id)
+                    data_time3.append(event.timestamp)
                         
                     
-                    data_ch3 = {"data": tempdata_list3, "identity": data_ID, "time": data_time}                 
+                    data_ch3 = {"data": tempdata_list3, "identity": data_ID3, "time": data_time3}                 
                     
                 elif channel == 4:
                     tempdata_list4.append(event.adc_data[boardID][channel]) #event.adc_data[boardID][boardChannel] -> Board nr. 3059 mit Input auf Channel 1
                     list_length = len(tempdata_list4)
-                    data_ID.append(event.event_id)
-                    data_time.append(event.timestamp)
+                    data_ID4.append(event.event_id)
+                    data_time4.append(event.timestamp)
                         
                     
-                    data_ch4 = {"data": tempdata_list4, "identity": data_ID, "time": data_time}                 
+                    data_ch4 = {"data": tempdata_list4, "identity": data_ID4, "time": data_time4}                 
                     
                 
             
@@ -116,30 +126,58 @@ def histogram(maxima, bins, title, xtitle):
 
     return hist_data
 
-def savehistogram(maxima, bins, title, xtitle):
-    plt.title(title)
-    plt.xlabel(xtitle)
-    plt.ylabel("Counts")
-    plt.hist(maxima[::-1],bins=bins)
-    
-    ax = plt.gca()
-    p = ax.patches
-    energy = [patch.get_xy() for patch in p]
-    counts = [patch.get_height() for patch in p]    
-    hist_data = {"energy": energy, "counts": counts}
-    
-    plt.yscale('log')
-    plt.show()
-    
+def savehistogram(hist_data):    
+    ##### saving 
     dir_input = input("Enter the desired directory for your file: ")
     filename = input("enter a filename (without .*)")
 
     filepath = "{}{}{}".format(dir_input, os.sep, filename)
     filepath = os.sep.join([dir_input, filename])
-
-    return hist_data
     
-    return 0
+    tupleenergy = hist_data["energy"]
+    energy =[]
+    
+    for i in range(len(tupleenergy)):
+        energy.append(tupleenergy[i][0])
+        
+    hist_data["energy"] = energy
+    width = 20
+    delim='\t'
+    column1 = '-'
+    order = ['energy', 'counts']
+    
+    with open( filepath, 'w' ) as f:
+        writer, w = csv.writer(f, delimiter=delim), []
+        head = ['{!s:{}}'.format(column1,width)]
+        
+        for i in order:    
+            head.append('{!s:{}}'.format(i,width))
+            
+        writer.writerow(head)            
+        
+        for i in range(len(tupleenergy)):
+            row = ['{!s:{}}'.format(i,width)]
+            for k in order:
+                temp = hist_data[k][i]
+                row.append('{!s:{}}'.format(temp,width))
+
+            writer.writerow(row)
+    
+    return print("your file has been saved")
+
+    # fmt = lambda s: '{!s:{}}'.format(s, width)
+    # with open(filepath, 'w+') as f:
+    #     writer = csv.writer(f, delimiter=delim)
+    #     writer.writerow([fmt(column1)] + [fmt(s) for s in order])
+        
+    #     for i in sorted(hist_data.keys()):
+    #       row = []
+    #       #row = ['{!s:{}}'.format(i,width)]
+    #       for k in order: row.append('{!s:{}}'.format(hist_data[i][k],width))
+    #       writer.writerow(row)
+    
+    # return print("your file has been saved")
+    
 
 def peakposition(x):
     peaks, _ = find_peaks(x, prominence=10)
@@ -182,7 +220,7 @@ def save_data(data):
 # filepath = "{}{}{}".format(dir_input, os.sep, filename)
 # filepath = os.sep.join([dir_input, filename])
 
-filepath = 'C:/Users/admin/Desktop/pydrs4/tests/2ch100k.bin'
+filepath = 'C:/Users/admin/Desktop/pydrs4/tests/2channels.bin'
 print(filepath)
 
 with DRS4BinaryFile(filepath) as f:
@@ -226,19 +264,26 @@ with DRS4BinaryFile(filepath) as f:
                 print('you chose histogram')
                 title = input("choose a title for your plot: ")
                 bins = int(input("choose the number of binaries: "))
-                save = input("Do you want to save the plot and data of the histogram?  'yes' or 'no': ")
+
                 
                 baseline = 34000
                 
                 data_corr = -np.array(temp_data['data'])+2*baseline
                 maxcounts = maxvalue(data_corr)
                 
-                if save == 'no':
-                    hist_data = histogram(maxcounts, bins, title, 'channel')
+                hist_data = histogram(maxcounts, bins, title, 'channel')
+                
+                save = input("Do you want to save the plot and data of the histogram?  'yes' or 'no': ")
+                
+                if save == 'yes':
+                    savehistogram(hist_data)
                     
-                elif save == 'yes':
-                    hist_data = savehistogram(maxcounts, bins, title, 'channel')
-            
+                else:
+                    operations = ['histogram', 'save_data', 'save_hist', 'next_channel', 'finish']
+                    print("Caution: If next_channel or finish are selected current data can be overwritten and data might be lost!")
+                    print("Available operations: ", operations)
+                    command = input("choose an operation: ")
+                    
             elif command == 'save_data':
                 print('you chose save_data')
                 save_data(data)
